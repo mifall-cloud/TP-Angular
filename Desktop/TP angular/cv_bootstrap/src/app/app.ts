80637e6aa9a4e16b596839797dcd6533137df5fe
feress <mifall@ept.sn>
2026-01-02 02:48:22 +0000

    autre composante

--- Desktop/TP angular/cv_bootstrap/src/app/app.ts
+++ Desktop/TP angular/cv_bootstrap/src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Entete } from './entete/entete';
+import { Main } from './main/main';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
   standalone: true,
+  imports: [
+    RouterOutlet,
+    Entete,
+    Main,
+  ],
 })
 export class App {
   protected readonly title = signal('cv_bootstrap');
---
+++ Desktop/TP angular/cv_bootstrap/src/app/main/main.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-cv',
+  imports: [],
+  templateUrl: './main.html',
+  styleUrl: './main.scss',
+  standalone: true,
+})
+export class Main {
+
+}
